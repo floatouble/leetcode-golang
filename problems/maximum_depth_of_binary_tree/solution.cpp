77ef3dcb83f26cf43fee294d478ddcc92389854c
floatouble <108622432+floatouble@users.noreply.github.com>
2019-02-08 18:29:44 +0000

    Sync LeetCode submission - Maximum Depth of Binary Tree (cpp)

--- problems/maximum_depth_of_binary_tree/solution.cpp
+++ problems/maximum_depth_of_binary_tree/solution.cpp
@@ -10,10 +10,10 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        if (root == nullptr){
+        if (root == nullptr) {
             return 0;
         }
         
-        return std::max(maxDepth(root->left), maxDepth(root->right)) + 1;   
+        return std::max(maxDepth(root->left) , maxDepth(root->right))+1;
     }
 };
