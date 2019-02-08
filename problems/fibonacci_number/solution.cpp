acdef7e5b1cfa55419ef73b418b8e0f169c88681
floatouble <108622432+floatouble@users.noreply.github.com>
2019-02-08 18:09:51 +0000

    Sync LeetCode submission - Fibonacci Number (cpp)

--- problems/fibonacci_number/solution.cpp
+++ problems/fibonacci_number/solution.cpp
@@ -1,9 +1,17 @@
 class Solution {
 public:
+    std::unordered_map<int, int> m;
     int fib(int N) {
-        if (N < 2) {
-            return N;
+        if (m.count(N)) {
+            return m[N];
         }
-        return fib(N-1) + fib(N-2);
+        
+        if (N < 2) {
+            m[N] = N;
+            return m[N];
+        }
+        
+        m[N] = fib(N-1) + fib(N-2);
+        return m[N];
     }
 };
