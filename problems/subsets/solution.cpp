f230db07def3e5b841e4c8d3a2919a45fea4b964
floatouble <108622432+floatouble@users.noreply.github.com>
2019-05-06 16:47:04 +0000

    Sync LeetCode submission - Subsets (cpp)

--- problems/subsets/solution.cpp
+++ problems/subsets/solution.cpp
@@ -1,27 +1,24 @@
 class Solution {
+    void generateSubset(std::vector<int> &arr, 
+                        std::vector<int> current, int index,
+                        std::vector<std::vector<int>> &result)
+{
+	if (index == arr.size())
+	{
+		result.push_back(current);
+		return;
+	}
+
+	current.push_back(arr[index]);
+	generateSubset(arr, current, index + 1, result);
+	current.pop_back();
+	generateSubset(arr, current, index + 1, result);
+}
 public:
-    void subsets(const std::vector<int> &nums, int currentIndex, std::vector<int> &subsetSoFar,
-                 std::vector<std::vector<int>> &result) {
-        if (currentIndex == nums.size()) {
-            result.push_back(subsetSoFar);
-            return;
-        }        
-        
-        // include nums[currentIndex] in the subset
-        subsetSoFar.push_back(nums[currentIndex]);
-        subsets(nums, currentIndex+1, subsetSoFar, result);
-        
-        // exclude nums[currentIndex] from the subset
-        subsetSoFar.pop_back();
-        subsets(nums, currentIndex+1, subsetSoFar, result);        
-    }
-    
-    vector<vector<int>> subsets(vector<int>& nums) {
-        std::vector<std::vector<int>> result;
-        std::vector<int> subsetSoFar;
-        int currentIndex = 0;
-        
-        subsets(nums, currentIndex, subsetSoFar, result);
-        return result;        
+    vector<vector<int>> subsets(vector<int>& arr) {
+        vector<vector<int>> result;
+        vector<int> current;
+        generateSubset(arr, current, 0, result);
+        return result;
     }
 };
