f225fc456fc817fb7adc6da0ebf8310ba2f685e3
floatouble <108622432+floatouble@users.noreply.github.com>
2022-04-05 14:39:29 +0000

    Sync LeetCode submission - Two Sum (golang)

--- problems/two_sum/solution.go
+++ problems/two_sum/solution.go
@@ -1,13 +1,12 @@
 func twoSum(nums []int, target int) []int {
     m := make(map[int]int)
-
-	for i, v := range nums {
-		diff := target - v
-		if x, ok := m[diff]; ok {
-			return []int{x, i}
-		}
-		m[v] = i
-	}
-	return []int{}
     
+    for i, v := range nums {
+        if x, ok := m[target-v]; ok {
+            return []int{x, i}
+        }
+        
+        m[v] = i
+    }
+    return []int{-1, -1}
 }
