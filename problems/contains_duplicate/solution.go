438e499af1a6468e8d2387168c45554fbbe48191
floatouble <108622432+floatouble@users.noreply.github.com>
2022-04-05 14:45:20 +0000

    Sync LeetCode submission - Contains Duplicate (golang)

---
+++ problems/contains_duplicate/solution.go
@@ -0,0 +1,13 @@
+func containsDuplicate(nums []int) bool {
+    set := make(map[int]struct{})
+    
+    for _, v := range nums {
+        if _, ok := set[v]; ok {
+            return true
+        }
+        
+        set[v] = struct{}{}
+    }
+    
+    return false
+}
