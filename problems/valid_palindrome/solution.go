98fef279b2f3cc0c251b77e9cf15173e88c11b5d
floatouble <108622432+floatouble@users.noreply.github.com>
2022-06-03 14:14:49 +0000

    Sync LeetCode submission - Valid Palindrome (golang)

--- problems/valid_palindrome/solution.go
+++ problems/valid_palindrome/solution.go
@@ -1,29 +1,26 @@
-import "unicode"
-
 func isPalindrome(s string) bool {
-    toLower := unicode.ToLower
-    isLetterOrDigit := func (c rune) bool {
-        return unicode.IsNumber(c) || unicode.IsLetter(c)
-    }
-    
     i := 0
-    j := len(s)-1
+	j := len(s) - 1
     
-    for i <= j {
-        for i <=j && !isLetterOrDigit(rune(s[i])) {
-            i++
-        }
-        
-        for i <= j && !isLetterOrDigit(rune(s[j])) {
-            j--
-        }
-        
-        if i <= j && toLower(rune(s[i])) != toLower(rune(s[j])) {
-            return false
-        }
-        i++
-        j--
+    letterOrDigit:=func(x rune) bool{
+        return unicode.IsLetter(x) || unicode.IsDigit(x)
     }
-    return true
-    
+
+	s = strings.ToLower(s)
+	for i <= j {
+		for i <= j && !letterOrDigit(rune(s[i])) {
+			i++
+		}
+
+		for i <= j && !letterOrDigit(rune(s[j])) {
+			j--
+		}
+
+		if i <= j && s[i] != s[j] {
+			return false
+		}
+		i++
+		j--
+	}
+	return true
 }
